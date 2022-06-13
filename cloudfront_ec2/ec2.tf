565988e43be662edf3686ce9c415b4884eca91fe
subhasis020299 <subhasisdas125@gmail.com>
2022-06-13 16:26:04 +0000

    final touches

--- cloudfront_ec2/ec2.tf
+++ cloudfront_ec2/ec2.tf
@@ -1,11 +1,10 @@
-
 # creating ec2 instance :
 resource "aws_instance" "ec2" {
   ami                         = data.aws_ami.ubuntu_ami.id
   instance_type               = "t2.micro"
-  associate_public_ip_address = true
   user_data                   = data.template_cloudinit_config.user_data.rendered
-
+  
+  
   tags = merge(local.tags, {
     Name = "ec2-cloudfront"
   })
