1962bf25a8b9934f8ab63c4d5afd259a68f3e4f1
subhasis020299 <subhasisdas125@gmail.com>
2022-06-17 06:56:06 +0000

    ASG-ALB integration

--- cloudfront_alb/alb.tf
+++ cloudfront_alb/alb.tf
@@ -1,27 +1,49 @@
+# Create Security Group for ALB Ingress :
+resource "aws_security_group" "alb_ingress" {
+  name = "alg_http_ingress"
+  vpc_id      = module.vpc.vpc_id
+  
+  # Allow ingress to http port 80 from anywhere
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 # Create ALB :
-resource "aws_lb" "alb" {
-  name               = "tf-cloudfront-alb"
-  internal           = false
-  load_balancer_type = "application"
-  subnets            = module.vpc.public_subnets
-
+resource "aws_lb" "alb" {  
+  name            = "tf-cloudfront-alb"  
+  subnets         = module.vpc.public_subnets
+  security_groups = [aws_security_group.alb_ingress.id]
+  internal        = false 
+  idle_timeout    = 60   
   tags = local.tags
-
-  depends_on = [
-    module.vpc
-  ]
 }
 
-# Create ALB Target Group :
-resource "aws_lb_target_group" "alb_tg" {
-  name        = "tf-cloudfront-alb-tg"
-  target_type = "alb"
-  port        = 80
-  protocol    = "TCP"
-  vpc_id      = module.vpc.id
-
-  depends_on = [
-    module.vpc
-  ]
+resource "aws_lb_target_group" "alb_tg" {  
+  name     = "tf-cloudfront-alb-tg"  
+  port     = "80"  
+  protocol = "HTTP"  
+  vpc_id   = module.vpc.vpc_id 
+  tags = local.tags
 }
 
+resource "aws_lb_listener" "alb_listener" {  
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80  
+  protocol          = "http"
+  
+  default_action {    
+    target_group_arn = aws_lb_target_group.alb_tg.arn
+    type             = "forward"  
+  }
+}
