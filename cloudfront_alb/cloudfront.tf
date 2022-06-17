5b0e5cc2f23f4f124af941fb5588b0e7ea692f70
subhasis020299 <subhasisdas125@gmail.com>
2022-06-17 13:11:15 +0530

    added cloudfront distribution

---
+++ cloudfront_alb/cloudfront.tf
@@ -0,0 +1,50 @@
+#creating CF distribution :
+resource "aws_cloudfront_distribution" "cf_dist" {
+  enabled             = true
+  aliases             = [var.domain_name]
+
+  origin {
+    domain_name = aws_lb.alb.dns_name
+    origin_id   = aws_lb.alb.dns_name
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods         = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id       = aws_lb.alb.dns_name
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      headers      = []
+      query_string = true
+
+      cookies {
+        forward = "all"
+      }
+    }
+
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["IN", "US", "CA"]
+    }
+  }
+
+  tags = local.tags
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate.cert.arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2018"
+  }
+}
+
