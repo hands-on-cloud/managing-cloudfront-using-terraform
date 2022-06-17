15c822f963b335b031863911bd573968e5af91f3
subhasis020299 <subhasisdas125@gmail.com>
2022-06-17 11:08:01 +0530

    vpc module fix

--- cloudfront_alb/vpc.tf
+++ cloudfront_alb/vpc.tf
@@ -2,16 +2,16 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "tf-cloudfront-alb-demo-vpc"
-  cidr = local.vpc_cidr
+  cidr = var.vpc_cidr
 
   azs = ["${var.region}a", "${var.region}b"]
   public_subnets = [
-    cidrsubnet(local.vpc_cidr, 8, 0),
-    cidrsubnet(local.vpc_cidr, 8, 1)
+    cidrsubnet(var.vpc_cidr, 8, 0),
+    cidrsubnet(var.vpc_cidr, 8, 1)
   ]
   private_subnets = [
-    cidrsubnet(local.vpc_cidr, 8, 2),
-    cidrsubnet(local.vpc_cidr, 8, 3)
+    cidrsubnet(var.vpc_cidr, 8, 2),
+    cidrsubnet(var.vpc_cidr, 8, 3)
   ]
 
   enable_nat_gateway   = true
