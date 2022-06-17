a8c67be6be27b7d043959aa1143a53b339718496
subhasis020299 <subhasisdas125@gmail.com>
2022-06-17 05:27:44 +0000

    minor changes

--- cloudfront_alb/asg.tf
+++ cloudfront_alb/asg.tf
@@ -4,7 +4,7 @@ resource "aws_launch_configuration" "asg_launch_conf" {
   name_prefix   = "tf-cloufront-alb-demo-"
   image_id      = data.aws_ami.ubuntu_ami.id
   instance_type = "t2.micro"
-  user_data = data.template_cloudinit_config.user_data.rendered
+  user_data     = data.template_cloudinit_config.user_data.rendered
 
   lifecycle {
     create_before_destroy = true
@@ -15,6 +15,12 @@ resource "aws_launch_configuration" "asg_launch_conf" {
 resource "aws_autoscaling_group" "asg" {
   name                 = "tf-cloudfront-alb-asg"
   launch_configuration = aws_launch_configuration.asg_launch_conf.name
+  min_size             = 1
+  max_size             = 3
+  
+  lifecycle {
+    create_before_destroy = true
+  }
 
   depends_on = [
     aws_launch_configuration.asg_launch_conf
